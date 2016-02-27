c0502d24a3bb87e33f97aa8b0ea34b1339aad3b0
MrEASY15 <ich@goetzuli.com>
2016-02-27 20:16:30 +0100

    Added files via upload

---
+++ runtastic/module.php
@@ -0,0 +1,91 @@
+<?
+
+	class Runtastic extends IPSModule
+	{
+		
+		private $imagePath;
+		
+		public function __construct($InstanceID)
+		{
+			//Never delete this line!
+			parent::__construct($InstanceID);
+			
+			//You can add custom code below.
+			//$this->imagePath = "media/radar".$InstanceID.".gif";
+			
+		}
+		
+		public function Create()
+		{
+			//Never delete this line!
+			parent::Create();
+			
+			//$this->RegisterPropertyString("area", "dsch");
+			//$this->RegisterPropertyInteger("homeX", 324);
+			//$this->RegisterPropertyInteger("homeY", 179);
+			//$this->RegisterPropertyInteger("homeRadius", 10);
+			
+		}		
+	
+		public function ApplyChanges()
+		{
+			//Never delete this line!
+			parent::ApplyChanges();
+			
+			// Integer
+			$this->RegisterVariableInteger("id", "Aktivitaets-ID");
+			$this->RegisterVariableInteger("type_id", "Aktivitaets-Typ-ID");
+			$this->RegisterVariableInteger("duration", "Dauer");
+			$this->RegisterVariableInteger("distance", "Strecke");
+			$this->RegisterVariableInteger("kcal", "kcal");
+			$this->RegisterVariableInteger("heartrate_avg", "Durchschnittspuls");
+			$this->RegisterVariableInteger("heartrate_max", "Maximalpuls");
+			$this->RegisterVariableInteger("weather_id", "Wetter-ID");
+			$this->RegisterVariableInteger("feeling_id", "Feeling-ID");
+			$this->RegisterVariableInteger("surface_id", "Oberflaechenbeschaffenheits-ID");
+			$this->RegisterVariableInteger("year", "Jahr");
+			$this->RegisterVariableInteger("month", "Monat"); // kommt als String
+			$this->RegisterVariableInteger("day", "Tag"); // kommt als String
+			$this->RegisterVariableInteger("hour", "Stunde");
+			$this->RegisterVariableInteger("minutes", "Minute");
+			$this->RegisterVariableInteger("seconds", "Sekunde");
+			
+			
+			// Strings
+			$this->RegisterVariableString("type", "Aktivitaets-Typ");
+			$this->RegisterVariableString("surface", "Oberflaechenbeschaffenheit");
+			$this->RegisterVariableString("weather", "Wetter");
+			$this->RegisterVariableString("feeling", "Feeling");
+			$this->RegisterVariableString("notes", "Notizen");
+			$this->RegisterVariableString("page_url", "page_url");
+			$this->RegisterVariableString("create_route_url", "create_route_url");
+			$this->RegisterVariableString("map_url", "map_url");
+			
+			// Floats
+			$this->RegisterVariableFloat("pace", "Aktivitaets-Pace");
+			$this->RegisterVariableFloat("speed", "Geschwindigkeit"); // kommt als String
+			$this->RegisterVariableFloat("elevation_gain", "Maximaler Höhenunterschied");
+			$this->RegisterVariableFloat("elevation_loss", "Höhenunterschied");
+
+			
+		}
+	
+		/**
+		* This function will be available automatically after the module is imported with the module control.
+		* Using the custom prefix this function will be callable from PHP and JSON-RPC through:
+		*
+		* UWZ_RequestInfo($id);
+		*
+		*/
+		public function RequestInfo()
+		{
+		
+
+			SetValue($this->GetIDForIdent("TestVariable"), 15);
+//			SetValue($this->GetIDForIdent("RainValue"), $regenmenge);
+			
+		}
+	
+	}
+
+?>
