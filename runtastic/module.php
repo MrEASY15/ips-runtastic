bffb68379a38df0163df788a6ca3b938ed26e6c3
MrEASY15 <ich@goetzuli.com>
2016-03-06 12:43:56 +0100

    Update module.php
    Variablen page_url, map_url und create_route_url beinhalten nun vollständigen Pfad.
    Profilerstellung sollte nun korrekt erfolgen.

--- runtastic/module.php
+++ runtastic/module.php
@@ -133,9 +133,9 @@
 			SetValue($this->GetIDForIdent("feeling_id"), $activities[0]->feeling_id);
 			SetValue($this->GetIDForIdent("surface_id"), $activities[0]->surface_id);
 			SetValue($this->GetIDForIdent("notes"), $activities[0]->notes);
-			SetValue($this->GetIDForIdent("page_url"), "http://www.runtastic.com/" . $activities[0]->page_url);
-			SetValue($this->GetIDForIdent("map_url"), "http://www.runtastic.com/" . $activities[0]->map_url);
-			SetValue($this->GetIDForIdent("create_route_url"), "http://www.runtastic.com/" . $activities[0]->create_route_url);
+			SetValue($this->GetIDForIdent("page_url"), "http://www.runtastic.com" . $activities[0]->page_url);
+			SetValue($this->GetIDForIdent("map_url"), "http://www.runtastic.com" . $activities[0]->map_url);
+			SetValue($this->GetIDForIdent("create_route_url"), "http://www.runtastic.com" . $activities[0]->create_route_url);
 			
 			// Datum und Uhrzeit
 			SetValue($this->GetIDForIdent("year"), $activities[0]->date->year);
